4fda8fb6bcdf1f144d7e49f214fb0f12f367c500
Christopher Mogler <mogler@weissedv.de>
2020-07-01 09:58:34 +0200

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,51 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.sql.*;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
+    static Connection conn = null;
 
     public static void main(String[] args) {
-        launch(args);
+        // Beginen Try Catch
+        try{
+            //Initalisiere den Treiber
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Bau eine Verbindung auf mit dem Connection String
+            conn = DriverManager.getConnection("jdbc:mysql://172.16.16.3/GP19_1?user=GP-User&password=1234");
+
+            //Hier wird nacher das Ergebnis (Tabelle) gespeichert.
+            ResultSet rs = null;
+
+            // Dieses Objekt führt den SQL code aus
+            Statement stmt = conn.createStatement();
+
+            // Führe SQL Code aus
+            if (stmt.execute("SELECT Firstname, Lastname, Value FROM Worker \n" +
+                    "left join WorkerSalary on Worker.ID = WorkerSalary.WorkerID")) {
+                // Setze die Tablle in die lokale Variable
+                rs = stmt.getResultSet();
+                // Schreibe den Kopfsatz in die Console
+                System.out.println("Name \tLastname \tGehalt");
+                System.out.println("--------------------------");
+                while (rs.next()) {
+                    // Gehe jede Zeile einzeln durch
+                    String firstname = rs.getString("Firstname"); //Hole die Werte der Spalte
+                    String lastname = rs.getString("Lastname");
+                    Double money = rs.getDouble("Value");
+                    //Gebe die Ergebnisse aus
+                    System.out.println(firstname + " \t" + lastname + " \t\t" + money);
+                }
+            }
+
+        }
+        catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException: " + ex.getMessage());
+        }
+        catch (SQLException ex){
+            System.out.println("SQLExcpetion: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
     }
 }
